9562ea67976d26aa31bbff83e6ee755a932e430c
YiqingDing <yiqingd@stanford.edu>
2018-06-22 14:40:30 -0700

    Revert "ParameterUpdate"
    This reverts commit 749091a19a8a59bd9684642c8dddcba626d9c5f0.

--- Baseline/ProcessNoise.m
+++
@@ -1,9 +0,0 @@
-function Q = ProcessNoise(n)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Generate Process Noise Covariance
-% Input:
-%       n - Total number of ships
-% Ouput:
-%       Q - Noise covariance (unit: km)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Q = 1e-12*eye(6*n);
